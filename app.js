de714c707e208473a7f680708a98c633820695f8
Evgeniievg <evgeniy.litvinv01@gmail.com>
2023-09-23 14:47:47 +0400

    update cors

--- app.js
+++ app.js
@@ -24,7 +24,10 @@ mongoose.connect(mongoUrl, { useNewUrlParser: true })
     console.log(`Произошла ошибка при установлении связи с MongoDB: ${error}`);
   });
 
-app.use(cors());
+app.use(cors({
+  origin: ['http://localhost:3000'],
+  credentials: true,
+}));
 
 app.use(helmet());
 app.use(cookies());
